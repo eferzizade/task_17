ffe0f0ecf17bdeb2e233f3b07695128cbe011b7e
eferzizade <eferzizade@std.beu.edu.az>
2025-12-09 23:06:49 +0400

    color filters

---
+++ Task_17/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Task_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            panel8 = new Panel();
+            pictureBox1 = new PictureBox();
+            selectbtn = new Button();
+            denomicbtn = new Button();
+            dramaticbtn = new Button();
+            freezingbtn = new Button();
+            holybtn = new Button();
+            naturlbtn = new Button();
+            button7 = new Button();
+            pictureBox2 = new PictureBox();
+            savebtn = new Button();
+            exitbtn = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkSlateGray;
+            panel1.Location = new Point(12, 86);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(892, 27);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DarkSlateGray;
+            panel2.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            panel2.Location = new Point(627, 184);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(265, 27);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.DarkSlateGray;
+            panel3.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            panel3.Location = new Point(627, 278);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(265, 27);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.DarkSlateGray;
+            panel4.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            panel4.Location = new Point(627, 477);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(265, 27);
+            panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.DarkSlateGray;
+            panel5.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            panel5.Location = new Point(627, 380);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(265, 27);
+            panel5.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.DarkSlateGray;
+            panel6.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            panel6.Location = new Point(627, 666);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(265, 27);
+            panel6.TabIndex = 6;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.DarkSlateGray;
+            panel7.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            panel7.Location = new Point(627, 572);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(265, 27);
+            panel7.TabIndex = 5;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.DarkSlateGray;
+            panel8.Location = new Point(599, 119);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(33, 571);
+            panel8.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._1496968502_jd24_84668;
+            pictureBox1.Location = new Point(54, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(113, 68);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // selectbtn
+            // 
+            selectbtn.BackColor = Color.Teal;
+            selectbtn.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            selectbtn.Location = new Point(196, 24);
+            selectbtn.Name = "selectbtn";
+            selectbtn.Size = new Size(205, 46);
+            selectbtn.TabIndex = 8;
+            selectbtn.Text = "Select image";
+            selectbtn.UseVisualStyleBackColor = false;
+            selectbtn.Click += selectbtn_Click;
+            // 
+            // denomicbtn
+            // 
+            denomicbtn.BackColor = Color.Teal;
+            denomicbtn.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            denomicbtn.Location = new Point(659, 132);
+            denomicbtn.Name = "denomicbtn";
+            denomicbtn.Size = new Size(205, 46);
+            denomicbtn.TabIndex = 9;
+            denomicbtn.Text = "Demonic";
+            denomicbtn.UseVisualStyleBackColor = false;
+            denomicbtn.Click += denomicbtn_Click;
+            // 
+            // dramaticbtn
+            // 
+            dramaticbtn.BackColor = Color.Teal;
+            dramaticbtn.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            dramaticbtn.Location = new Point(659, 226);
+            dramaticbtn.Name = "dramaticbtn";
+            dramaticbtn.Size = new Size(205, 46);
+            dramaticbtn.TabIndex = 10;
+            dramaticbtn.Text = "Dramatic";
+            dramaticbtn.UseVisualStyleBackColor = false;
+            dramaticbtn.Click += dramaticbtn_Click;
+            // 
+            // freezingbtn
+            // 
+            freezingbtn.BackColor = Color.Teal;
+            freezingbtn.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            freezingbtn.Location = new Point(659, 328);
+            freezingbtn.Name = "freezingbtn";
+            freezingbtn.Size = new Size(205, 46);
+            freezingbtn.TabIndex = 11;
+            freezingbtn.Text = "Freezing";
+            freezingbtn.UseVisualStyleBackColor = false;
+            freezingbtn.Click += freezingbtn_Click;
+            // 
+            // holybtn
+            // 
+            holybtn.BackColor = Color.Teal;
+            holybtn.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            holybtn.Location = new Point(659, 425);
+            holybtn.Name = "holybtn";
+            holybtn.Size = new Size(205, 46);
+            holybtn.TabIndex = 12;
+            holybtn.Text = "Holy";
+            holybtn.UseVisualStyleBackColor = false;
+            holybtn.Click += holybtn_Click;
+            // 
+            // naturlbtn
+            // 
+            naturlbtn.BackColor = Color.Teal;
+            naturlbtn.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            naturlbtn.Location = new Point(659, 520);
+            naturlbtn.Name = "naturlbtn";
+            naturlbtn.Size = new Size(205, 46);
+            naturlbtn.TabIndex = 13;
+            naturlbtn.Text = "Natural";
+            naturlbtn.UseVisualStyleBackColor = false;
+            naturlbtn.Click += naturlbtn_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Teal;
+            button7.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            button7.Location = new Point(659, 614);
+            button7.Name = "button7";
+            button7.Size = new Size(205, 46);
+            button7.TabIndex = 14;
+            button7.Text = "Sunny";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.picture_icon_129402;
+            pictureBox2.Location = new Point(39, 236);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(260, 346);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 15;
+            pictureBox2.TabStop = false;
+            // 
+            // savebtn
+            // 
+            savebtn.BackColor = Color.Teal;
+            savebtn.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            savebtn.Location = new Point(348, 307);
+            savebtn.Name = "savebtn";
+            savebtn.Size = new Size(172, 67);
+            savebtn.TabIndex = 16;
+            savebtn.Text = "Save image";
+            savebtn.UseVisualStyleBackColor = false;
+            savebtn.Click += savebtn_Click;
+            // 
+            // exitbtn
+            // 
+            exitbtn.BackColor = Color.Teal;
+            exitbtn.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold);
+            exitbtn.Location = new Point(348, 477);
+            exitbtn.Name = "exitbtn";
+            exitbtn.Size = new Size(172, 67);
+            exitbtn.TabIndex = 17;
+            exitbtn.Text = "Exit";
+            exitbtn.UseVisualStyleBackColor = false;
+            exitbtn.Click += button9_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Trebuchet MS", 16.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(662, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(190, 37);
+            label1.TabIndex = 18;
+            label1.Text = "Color filters:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Teal;
+            ClientSize = new Size(892, 690);
+            Controls.Add(label1);
+            Controls.Add(exitbtn);
+            Controls.Add(savebtn);
+            Controls.Add(pictureBox2);
+            Controls.Add(button7);
+            Controls.Add(naturlbtn);
+            Controls.Add(holybtn);
+            Controls.Add(freezingbtn);
+            Controls.Add(dramaticbtn);
+            Controls.Add(denomicbtn);
+            Controls.Add(selectbtn);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel8);
+            Controls.Add(panel6);
+            Controls.Add(panel7);
+            Controls.Add(panel4);
+            Controls.Add(panel5);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Photo";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private PictureBox pictureBox1;
+        private Button selectbtn;
+        private Button denomicbtn;
+        private Button dramaticbtn;
+        private Button freezingbtn;
+        private Button holybtn;
+        private Button naturlbtn;
+        private Button button7;
+        private PictureBox pictureBox2;
+        private Button savebtn;
+        private Button exitbtn;
+        private Label label1;
+    }
+}
---
+++ Task_17/Form1.cs
@@ -0,0 +1,98 @@
+﻿namespace Task_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Proqramdan çıxmaq istədiyinizə əminsiniz?",
+                "Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        Bitmap bmp;
+        private void selectbtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                bmp = new Bitmap(path);
+                pictureBox2.Image = bmp;
+            }
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sv = new SaveFileDialog();
+            if (DialogResult.OK == sv.ShowDialog())
+            {
+                string path = sv.FileName;
+                bmp.Save(path);
+                MessageBox.Show("Image saved successfully!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private Bitmap Filter(Bitmap original, int addR, int addG, int addB)
+        {
+            Bitmap newBmp = new Bitmap(original.Width, original.Height);
+
+            for (int x = 0; x < original.Width; x++)
+            {
+                for (int y = 0; y < original.Height; y++)
+                {
+                    Color clr = original.GetPixel(x, y);
+
+                    int r = Math.Max(0, Math.Min(255, clr.R + addR));
+                    int g = Math.Max(0, Math.Min(255, clr.G + addG));
+                    int b = Math.Max(0, Math.Min(255, clr.B + addB));
+
+                    newBmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            return newBmp;
+        }
+
+        private void denomicbtn_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Filter(bmp, 50, -20, -20);
+        }
+
+        private void dramaticbtn_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Filter(bmp, -20, -20, 50);
+        }
+
+        private void freezingbtn_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Filter(bmp, -10, 20, 60);
+        }
+
+        private void holybtn_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Filter(bmp, 40, 40, 20);
+        }
+
+        private void naturlbtn_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Filter(bmp, 10, 20, 10);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Filter(bmp, 60, 40, -10);
+        }
+    }
+}
